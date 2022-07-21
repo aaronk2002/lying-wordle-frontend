3af6965484685865da5537a20e6c290946a4288d
aaronk2002 <aaronakj2002@gmail.com>
2022-07-21 12:08:54 -0400

    Connected to Heroku

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 }
 
 function callApi() {
-    fetch('http://localhost:3001/details', { method: 'GET' })
+    fetch('https://lying-wordle-server.herokuapp.com/', { method: 'GET' })
         .then(data => data.json())
         .then(json => alert(JSON.stringify(json)))
 }
