47bc4a3537d71f124a227eca10bebd7e29b0bdff
aaronk2002 <aaronakj2002@gmail.com>
2022-07-24 17:25:20 -0400

    Put import useEffect in Menu and ModifyDB

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { Link } from "react-router-dom";
 import WordTiles from "../Components/WordTiles.js";
 import "../Styles/FlexPosition.css";
--- src/Pages/ModifyDB.js
+++ src/Pages/ModifyDB.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ModifyDB = () => {
     const [myWord, setMyWord] = useState([]);
