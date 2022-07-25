1ac5f76b3356270a33e72c2871932ad338401b14
aaronk2002 <aaronakj2002@gmail.com>
2022-07-25 16:48:58 -0400

    Attempt to fix tiles on mobile 7

--- src/Components/LetterTile.js
+++ src/Components/LetterTile.js
@@ -2,14 +2,18 @@ import '../Styles/Colors.css';
 import '../Styles/Sizes.css';
 import '../Styles/FlexPosition.css';
 import '../Styles/Utilities.css';
+import {BrowserView, MobileView} from 'react-device-detect';
 
 // A component that makes one tile.
 function LetterTile(props) {
     return (<>
         <div className={props.color + ' square center ' + props.size}>
-            <div className='mobile-center center'>
-                <p className='remove-margin center'>{props.letter}</p>
-            </div>
+            <MobileView>
+                <div className='mobile-center center'>
+                    <p className='remove-margin center'>{props.letter}</p>
+                </div>
+            </MobileView>
+            <BrowserView>{props.letter}</BrowserView>
         </div>
     </>);
 }
